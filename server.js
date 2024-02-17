274ad0070303ec2e0a00653e1aa79ac0f0a91a17
Tedros Kahsu <tedkahcom@gmail.com>
2024-02-17 15:00:21 +0300

    Fix CROS issue

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const songsRouter = require("./routes/songs");
 
@@ -12,6 +13,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connetion error:", err));
 
+app.use(cors());
 // Routes
 app.use("/api/songs", songsRouter);
 
